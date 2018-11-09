49cd3c7e1b499801d25fb7f5c719fa737fede6ba
Eva G Alvarez <eva@pi245130.inv.usc.es>
2018-11-09 13:22:56 +0100

    Deleted unnecessary repetition of names while parsing arguments.

--- ARCHITECT.py
+++ ARCHITECT.py
@@ -25,14 +25,14 @@ parser.add_argument('-t', '--threads', default=1, dest='threads', type=int, help
 parser.add_argument('-o', '--outDir', default=os.getcwd(), dest='outDir', help='output directory. Default: current working directory.' )
 
 parser.add_argument('-wS', '--windowSize', default=1000000, dest='windowSize', type=int, help='Input bams will be analised in windows of this size. Default: 1000000' )
-parser.add_argument('-refs', '--refs', default="ALL", dest='refs', type=str, help='References to analyse from the bam file. Default: All references are analysed.')
-parser.add_argument('-SV', '--SV', default="INS,DEL,CLIPPING", dest='SV', type=str, help='Structural variants to look for within reads. INS: insertion, DEL: deletion, CLIPPING: clip reads. Default: INS, DEL, CLIPPING')
-parser.add_argument('-minMAPQ', '--minMAPQ', default=20, dest='minMAPQ', type=int, help='Minimum mapping quality requires for each read.')
-parser.add_argument('-minINDELlen', '--minINDELlen', default=50, dest='minINDELlen', type=int, help='Minimum indel length.')
-parser.add_argument('-minCLIPPINGlen', '--minCLIPPINGlen', default=500, dest='minCLIPPINGlen', type=int, help='Minimum length of clipping region for each read.')
-parser.add_argument('-maxBkpDist', '--maxBkpDist', default=50, dest='maxBkpDist', type=int, help='Maximum distance bewteen two adjacent breakpoints of the same cluster (applies only for those SVs which only one breakpoint (i.e. INS, CLIPPING)).')
-parser.add_argument('-minRootClusterSize', '--minRootClusterSize', default=2, dest='minRootClusterSize', type=int, help='Minimum number of reads of the first cluster found (before extending it).')
-parser.add_argument('-maxClusterDist', '--maxClusterDist', default=100, dest='maxClusterDist', type=int, help='Maximum distance bewteen different clusters.')
+parser.add_argument('--refs', default="ALL", dest='refs', type=str, help='References to analyse from the bam file. Default: All references are analysed.')
+parser.add_argument('--SV', default="INS,DEL,CLIPPING", dest='SV', type=str, help='Structural variants to look for within reads. INS: insertion, DEL: deletion, CLIPPING: clip reads. Default: INS, DEL, CLIPPING')
+parser.add_argument('--minMAPQ', default=20, dest='minMAPQ', type=int, help='Minimum mapping quality requires for each read.')
+parser.add_argument('--minINDELlen', default=50, dest='minINDELlen', type=int, help='Minimum indel length.')
+parser.add_argument('--minCLIPPINGlen', default=500, dest='minCLIPPINGlen', type=int, help='Minimum length of clipping region for each read.')
+parser.add_argument('--maxBkpDist', default=50, dest='maxBkpDist', type=int, help='Maximum distance bewteen two adjacent breakpoints of the same cluster (applies only for those SVs which only one breakpoint (i.e. INS, CLIPPING)).')
+parser.add_argument('--minRootClusterSize', default=2, dest='minRootClusterSize', type=int, help='Minimum number of reads of the first cluster found (before extending it).')
+parser.add_argument('--maxClusterDist', default=100, dest='maxClusterDist', type=int, help='Maximum distance bewteen different clusters.')
 
 args = parser.parse_args()
 bam = args.bam
