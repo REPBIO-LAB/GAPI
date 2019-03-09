6bd26f2635b2b311e2add243a7457583a391e636
Bernardo Rodríguez Martín <rodriguezmartinbernardo@gmail.com>
2019-03-09 23:44:01 +0100

    Added missing file

---
+++ databases.py
@@ -0,0 +1,65 @@
+'''
+Module 'databases' - Contains functions and classes to deal with different types of databases
+'''
+
+## DEPENDENCIES ##
+# External
+import subprocess
+
+# Internal
+import unix 
+
+## FUNCTIONS ##
+def buildRetrotransposonDb(fastaDir, includeTransduced, outDir):
+    '''
+    Build database containing retrotransposon related sequences (consensus sequences, transduced regions, ...)
+
+    Input:
+        1. fastaDir: Directory containing reference fasta files (retrotransposon consensus sequences, transduced regions...)
+        2. includeTransduced: Boolean to specify if include (True) or not (False) transduced regions from known source elements in the database
+        3. outDir: Output directory
+
+    Output:
+        1. retrotransposonDb: Fasta file containing retrotransposon related sequences 
+    '''
+
+    ## 0. Create logs directory
+    logDir = outDir + '/Logs'
+    unix.mkdir(logDir)
+
+    ## 1. Create database fasta file ##
+    retrotransposonDb = outDir + '/retrotransposonDb.fa'
+
+    # A) Include retrotransposon consensus sequences + transduced regions
+    if includeTransduced:
+
+        consensusDb = fastaDir + 'consensusDb.fa'
+        transducedDb = fastaDir + 'transducedDb.masked.fa'
+        files = [consensusDb, transducedDb]
+
+        with open(retrotransposonDb, 'w') as outFile:
+            # Iterate over each fasta and write fasta into output database
+            for f in files:
+                with open(f) as inFile:
+                    outFile.write(inFile.read())
+        
+    # B) Only include retrotransposon consensus sequences
+    else:
+        consensusDb = fastaDir + 'consensusDb.fa'
+
+        with open(retrotransposonDb, 'w') as outFile:
+            with open(consensusDb) as inFile:
+                outFile.write(inFile.read())
+
+    ## 2. Index retrotransposon database fasta file ##
+    index = outDir + '/retrotransposonDb.mmi'
+    err = open(logDir + '/index.err', 'w') 
+    command = 'minimap2 -k 10 -w 1 -d ' + index + ' ' + retrotransposonDb 
+    status = subprocess.call(command, stderr=err, shell=True)
+
+    if status != 0:
+        step = 'BUILD-DATABASE'
+        msg = 'Database indexing failed' 
+        log.step(step, msg)
+
+    return retrotransposonDb, index
