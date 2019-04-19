e4db42d159b11d2d11441fefafd3366a1faa2b3d
Eva G Alvarez <eva.gaal93@gmail.com>
2019-04-19 16:28:58 +0100

    Organizing bkp.py

--- databases.py
+++ databases.py
@@ -116,49 +116,38 @@ def buildVirusDb(fastaDir, outDir):
     return virusDb, index
 
 
-def buildRefIdentityDb(ref, beg, end, identity, specificIdentity, identityDbFasta, reference, side, outDir):
+def buildIdentityDb(metacluster, db, outDir):
 
-    region = ref +':'+ str(beg) +'-'+ str(end)
+    # Coger la identity del primer evento DISCORDANT que aparezca (pq los clipping no tienen identity)
+    identity = next(event.identity for event in metacluster.events if event.type == "DISCORDANT")
+    
+    specificIdentity = max(set([event.specificIdentity for event in metacluster.events if event.type == "DISCORDANT"]), key=[event.specificIdentity for event in metacluster.events if event.type == "DISCORDANT"].count)
 
-    # TODO: poner bien el outfile:
-    outFileRefRegion = outDir + '/' + str(beg) + '_' + side + '_reference_region.fa' 
-    # 1. Cojo el trozo de la ref que me interesa:
-    # TODO poner bien el status y todo eso
-    #err = open(logDir + '/index.err', 'w') 
-    command = 'samtools faidx  ' + reference + ' ' + region + ' -o ' + outFileRefRegion
-    status = subprocess.call(command, shell=True)
-    outFilespecificIdentity = outDir + '/' + str(beg) + '_' + side + '_specificIdentity.fa' 
     specificHeader = '"consensus|' + specificIdentity + '|' + identity + '"'
+
+    #dbSpecificIdentity = outDir + '/' + str(CLIPPING_clusterID) + '_specificIdentity.fa'
+    dbSpecificIdentity = outDir + '/' + str(metacluster.id) + '_specificIdentity.fa' 
+
+    # Build database con identity + ref
+    # TODO coger la identity que ya hemos reconocido, en lugar de toda la db!
     # 2. Cojo de la db de identities la secuencia que fue asignada como identity:
     # TODO poner bien el status y todo eso
     #err = open(logDir + '/index.err', 'w') 
-    command = 'samtools faidx  ' + identityDbFasta + ' ' + specificHeader + ' -o ' + outFilespecificIdentity
+    command = 'samtools faidx  ' + db + ' ' + specificHeader + ' -o ' + dbSpecificIdentity
     status = subprocess.call(command, shell=True)
-    refIdentityDb = outDir + '/' + str(beg) + '_' + side + '_refIdentityDb_region.fa' 
-    command = 'cat  ' + outFileRefRegion + ' ' + outFilespecificIdentity + ' > ' + refIdentityDb
-    status = subprocess.call(command, shell=True)
-    #if status != 0:
-        #step = 'BUILD-VIRUS-DATABASE'
-        #msg = 'Database indexing failed' 
-        #log.step(step, msg)
-    
     # indexo
-    refIdentityDbIndex = outDir + '/' + str(beg) + '_' + side + '_refIdentityDb_region.mmi' 
+    indexDbSpecificIdentity = dbSpecificIdentity.replace("_specificIdentity.fa", "_refIdentityDb.mmi")
+
     ## DESILENCIAAAAR
-    '''
     # TODO
     # ponerlo bien
     #err = open(logDir + '/index.err', 'w')
-    command = 'minimap2 -k 10 -w 1 -d ' + refIdentityDbIndex + ' ' + refIdentityDb 
+    command = 'minimap2 -k 10 -w 1 -d ' + indexDbSpecificIdentity + ' ' + dbSpecificIdentity 
     status = subprocess.call(command, shell=True)
 
-    
     if status != 0:
         step = 'BUILD-VIRUS-DATABASE'
         msg = 'Database indexing failed' 
         log.step(step, msg)
-        '''
-        
-        
 
-    return refIdentityDbIndex
+    return indexDbSpecificIdentity
