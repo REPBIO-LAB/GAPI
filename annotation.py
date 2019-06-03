95a4279ecf5f3aaeba58f2b333f8cfb0edfdaa02
Bernardo Rodríguez Martín <rodriguezmartinbernardo@gmail.com>
2019-06-03 23:07:08 +0200

    'annotation' module. New functions:
    - 'gene_annotation'. Perform gene-based annotation for a list of input events
    - 'create_annovar_input'. Write events intervals into a format compatible with annovar
    - 'read_annovar_out1'. Read annovar out1 and store info in a dictionary
    - 'run_annovar'. For each input entry perform gene-based annotation with Annovar

--- annotation.py
+++ annotation.py
@@ -2,7 +2,16 @@
 Module 'annotation' - Contains functions for the annotation of genomic intervals according to different annotation resources
 '''
 
-def annotate_repeats(events, repeatsDb, buffer):
+## DEPENDENCIES ##
+# External
+import os
+import subprocess
+
+# Internal
+import unix
+import formats
+
+def repeats_annotation(events, repeatsDb, buffer):
     '''
     For each input event assess if overlaps with an annotated repeat in the reference genome
 
@@ -12,7 +21,8 @@ def annotate_repeats(events, repeatsDb, buffer):
         3. buffer: number of base pairs to extend begin and end coordinates for each event prior assessing overlap
 
     Output:
-        1) events: list of input events with a new 'repeats' attribute set for each event. Attribute contains a not redundant list of overlapping repeats
+        New 'repeatAnnot' attribute set for each input event. 
+        'repeatAnnot' is a not redundant list of overlapping repeats
     '''
     ## Assess for each input event if it overlaps with an annotated repeat
     for event in events:
@@ -36,8 +46,143 @@ def annotate_repeats(events, repeatsDb, buffer):
         ## Generate repeats string and add to the input event
         # A) Event overlapping repeat
         if repeats:
-            event.repeats = ','.join(set(repeats))
+            event.repeatAnnot = ','.join(set(repeats))
 
         # B) Event NOT overlapping repeat
         else:    
-            event.repeats = None
+            event.repeatAnnot = None
+
+def gene_annotation(events, annovarDir, outDir):
+    '''
+    Perform gene-based annotation for a list of input events
+ 
+    Input: 
+        1. events: List containing input events to be annotated. Events should be objects containing ref, beg and end attributes.
+        2. annovarDir: Directory containing the two files used by ANNOVAR to perform gene based-annotation:
+                a) build_annot.txt     - Text file containing annotated transcript coordinates
+                b) build_annotMrna.fa  - Fasta containing annotated transcript sequences
+        3. outDir: Output directory
+
+    Output:
+    
+        New 'geneAnnot' attribute set for each input event. 
+        'geneAnnot' is a tuple(region,gene) 
+    '''
+
+    ## 1. Create output directory
+    unix.mkdir(outDir)
+
+    ## 2. Create input file containing events intervals for ANNOVAR 
+    create_annovar_input(events, 'events.annovar', outDir)
+    annovarInput = outDir + '/events.annovar'
+
+    ## 3. Annotate events intervals with ANNOVAR 
+    out1, out2 = run_annovar(annovarInput, annovarDir, outDir)
+
+    ## 4. Add gene annotation info to the events
+    # Read annovar output file into a dict
+    out1Dict = read_annovar_out1(out1)
+
+    # Add to each event gene annotation info
+    for event in events:
+
+        ## Add gene annotation info
+        name = event.ref + ':' + str(event.beg) + '-' + str(event.end)
+        event.geneAnnot = out1Dict[name]
+        
+    ## Do cleanup
+    unix.rm([outDir])
+
+
+def create_annovar_input(events, fileName, outDir):
+    '''
+    Write events intervals into a format compatible with annovar 
+
+    Input:
+        1. events: List containing input events. Events should be objects containing ref, beg and end attributes.
+        2. fileName: Output file name 
+        3. outDir: Output directory
+    '''
+    ## 1. Initialize BED 
+    BED = formats.BED()
+
+    ## 2. Add events intervals to the BED  
+    BED.lines = []
+    BED.structure = 'List'
+
+    # For each event
+    for event in events:
+
+        # Create BED entry:
+        name = event.ref + ':' + str(event.beg) + '-' + str(event.end)
+        fields = [event.ref, event.beg, event.end, name]
+        entry = formats.BED_line(fields)
+        
+        # Add entry to BED
+        BED.lines.append(entry)
+
+    ## 3. Write bed into a format compatible with annovar 
+    outFile = outDir + '/' + fileName
+    BED.write_annovar(outFile)
+
+
+def read_annovar_out1(out1):
+    '''
+    Read annovar out1 and store info in a dictionary
+
+    Input:
+        1. out1: Annovar output file 1 (region annotation for all the variants) 
+
+    Output:
+        1. out1Dict: Dictionary with comment field as keys and tuples(region,gene) as values        
+    '''
+
+    ## Open annovar out file
+    with open(out1, "r") as out1File:
+    
+        out1Dict = {}
+
+        # Read line by line adding the relevant info to the dict in each iteration
+        for line in out1File:
+
+            fields = line.split()
+            region = fields[0]
+            gene = fields[1]
+            name = fields[8] 
+            
+            out1Dict[name] = (region, gene)
+
+    return out1Dict
+            
+def run_annovar(inputFile, annovarDir, outDir):
+    '''
+    For each input entry perform gene-based annotation with Annovar
+
+    Input: 
+        1. inputFile: Annovar-like file containing regions to be annotated
+        2. annovarDir: Directory where annovar annotation files are located
+        3. outDir: Output directory
+
+    Output: 
+        1. out1: Annovar output file 1 (region annotation for all the variants) 
+        2. out2: Annovar output file 2 (amino acid changes as a result of the exonic variant) 
+
+    NOTE: perl annotate_variation.pl call should be available as 'ANNOVAR' environmental variable
+    NOTE: check annovar documentation for explanation about how to interpret output files:
+
+    http://annovar.openbioinformatics.org/en/latest/user-guide/gene/#output-file-1-refseq-gene-annotation
+    http://annovar.openbioinformatics.org/en/latest/user-guide/gene/#output-file-2-refseq-gene-annotation
+    '''
+
+    ## Run annovar in the input file
+    ANNOVAR = os.environ['ANNOVAR']
+    out = open(outDir + '/annovar.out', 'w') 
+    err = open(outDir + '/annovar.err', 'w') 
+    command = ANNOVAR + ' -buildver build -out ' + outDir + '/annovar -dbtype annot ' + inputFile + ' ' + annovarDir
+    status = subprocess.call(command, stdout=out, stderr=err, shell=True)
+
+    ## Return results
+    out1 = outDir + '/annovar.variant_function'
+    out2 = outDir + '/annovar.exonic_variant_function'
+
+    return out1, out2
