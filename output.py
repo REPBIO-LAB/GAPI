51267fac119452d6cdf76351931a056d58b0b858
Bernardo Rodríguez Martín <rodriguezmartinbernardo@gmail.com>
2019-06-20 16:06:45 +0200

    output module. Include coefficient of variation into the output tsv

--- output.py
+++ output.py
@@ -11,7 +11,7 @@ def write_INS(metaclustersBinDb, outDir):
     outFile = open(outFilePath, 'w')
 
     ## 2. Write header 
-    row = "#ref \t beg \t end \t binId \t insType \t family \t srcId \t status \t percResolved \t strand \t hits \t nbTotal \t nbTumour \t nbNormal \t nbINS \t nbDEL \t nbCLIPPING \t length \t insertSeq \n"
+    row = "#ref \t beg \t end \t binId \t insType \t family \t srcId \t status \t percResolved \t strand \t hits \t nbTotal \t nbTumour \t nbNormal \t nbINS \t nbDEL \t nbCLIPPING \t length \t cv \t insertSeq \n"
     outFile.write(row)
 
     ## 3. Write clusters 
@@ -20,22 +20,23 @@ def write_INS(metaclustersBinDb, outDir):
             
         binId = binDb.ref + ':' + str(binDb.beg) + '-' + str(binDb.end) 
 
-        # For each INS in the binDb
-        for INS in binDb.collect(['INS']):
+        # For each metacluster in the binDb
+        for metacluster in binDb.collect(['INS']):
 
             # Collect INS features
-            nbTotal, nbTumour, nbNormal, nbINS, nbDEL, nbCLIPPING = INS.nbEvents()
+            nbTotal, nbTumour, nbNormal, nbINS, nbDEL, nbCLIPPING = metacluster.nbEvents()
 
-            insType = INS.INS_features['insType'] if 'insType' in INS.INS_features else None
-            family = INS.INS_features['family'] if 'family' in INS.INS_features else None
-            srcId = INS.INS_features['srcId'] if 'srcId' in INS.INS_features else None
-            status = INS.INS_features['status'] if 'status' in INS.INS_features else None
-            percResolved = INS.INS_features['percResolved'] if 'percResolved' in INS.INS_features else None
-            strand = INS.INS_features['strand'] if 'strand' in INS.INS_features else None
-            hits = INS.INS_features['hits'] if 'hits' in INS.INS_features else None
+            insType = metacluster.INS_features['insType'] if 'insType' in metacluster.INS_features else None
+            family = metacluster.INS_features['family'] if 'family' in metacluster.INS_features else None
+            srcId = metacluster.INS_features['srcId'] if 'srcId' in metacluster.INS_features else None
+            status = metacluster.INS_features['status'] if 'status' in metacluster.INS_features else None
+            percResolved = metacluster.INS_features['percResolved'] if 'percResolved' in metacluster.INS_features else None
+            strand = metacluster.INS_features['strand'] if 'strand' in metacluster.INS_features else None
+            hits = metacluster.INS_features['hits'] if 'hits' in metacluster.INS_features else None
+            meanLen, cv = metacluster.subclusters['INS'].cv_len() if 'INS' in metacluster.subclusters else (None, None)
 
             # Write INS call into output file
-            row = "\t".join([INS.ref, str(INS.beg), str(INS.end), binId, str(insType), str(family), str(srcId), str(status), str(percResolved), str(strand), str(hits), str(nbTotal), str(nbTumour), str(nbNormal), str(nbINS), str(nbDEL), str(nbCLIPPING), str(INS.consensus.length), INS.consensus.pick_insert(), "\n"])
+            row = "\t".join([metacluster.ref, str(metacluster.beg), str(metacluster.end), binId, str(insType), str(family), str(srcId), str(status), str(percResolved), str(strand), str(hits), str(nbTotal), str(nbTumour), str(nbNormal), str(nbINS), str(nbDEL), str(nbCLIPPING), str(metacluster.consensus.length), str(cv), metacluster.consensus.pick_insert(), "\n"])
             outFile.write(row)
 
     ## Close output file ##
