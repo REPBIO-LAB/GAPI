6cd6d4ad56507f12353b7cb20b3f29c00285e5e2
Bernardo Rodríguez Martín <rodriguezmartinbernardo@gmail.com>
2019-07-18 13:10:51 +0200

    alignment module. New alignment_minimap2 function to align a set of sequences into a reference with minimap2

--- alignment.py
+++ alignment.py
@@ -39,20 +39,54 @@ def minimap2_presets(technology):
 
     return preset
 
+def alignment_minimap2(FASTA, index, outDir):
+    '''
+    Align a set of sequence into a reference with minimap2
+
+    Input:
+        1. FASTA: Path to FASTA file with sequences to align
+        2. index: Path to the the index of the reference in .mmi format (generated with minimap2)
+        3. outDir: Output directory
+
+    Output:
+        1. BAM: Path to sorted BAM file containing input sequences alignments or 'None' if alignment failed 
+    '''
+
+    ## 1. Align the sequences into the reference
+    # Use -Y to get soft clippings for supplementary alignments
+    SAM = outDir + '/alignments.sam'
+    err = open(outDir + '/align.err', 'w') 
+    command = 'minimap2 -Y -a -k15 -w10 ' + index + ' ' + FASTA + ' > ' + SAM
+    status = subprocess.call(command, stderr=err, shell=True)
+
+    if status != 0:
+        step = 'ALIGN'
+        msg = 'Local alignment failed' 
+        log.step(step, msg)
+        
+    ## 2. Convert SAM to sorted BAM
+    BAM = bamtools.SAM2BAM(SAM, outDir)
+
+    ## 3. Do cleanup
+    unix.rm([SAM])
+
+    return BAM
+
+
 def targeted_alignment_minimap2(FASTA, targetInterval, reference, outDir):
     '''
-    Align a set of sequences into a reference genome target region. 
+    Align a set of sequences into a reference target region. 
     
     Useful for doing local realignment of reads around SV breakpoints. Much faster than whole genome realignment
 
     Input:
         1. FASTA: Path to FASTA file with sequences to align
         2. targetInterval: Reference genome interval where sequences will be aligned. The interval must be provided as chr:beg-end.
-        3. reference: Path to the reference genome in fasta format. An index of the reference generated with samtools faidx must be located in the same directory
+        3. reference: Path to the reference sequences in fasta format. An index of the reference generated with samtools faidx must be located in the same directory
         4. outDir: Output directory
         
     Output:
-        1. BAM_sorted: Path to sorted BAM file containing input sequences alignments or 'None' if realignment failed 
+        1. BAM: Path to sorted BAM file containing input sequences alignments or 'None' if realignment failed 
     '''
     ## 0. Create logs directory
     logDir = outDir + '/Logs'
@@ -75,7 +109,6 @@ def targeted_alignment_minimap2(FASTA, targetInterval, reference, outDir):
     SAM = outDir + '/alignments.sam'
     err = open(logDir + '/align.err', 'w') 
     command = 'minimap2 -Y -a -k15 -w10 ' + target + ' ' + FASTA + ' > ' + SAM
-
     status = subprocess.call(command, stderr=err, shell=True)
 
     if status != 0:
@@ -86,6 +119,9 @@ def targeted_alignment_minimap2(FASTA, targetInterval, reference, outDir):
 
     ## 3. Convert SAM to sorted BAM
     BAM = bamtools.SAM2BAM(SAM, outDir)
+
+    ## 4. Do cleanup
+    unix.rm([target, SAM])
 
     return BAM    
 
