d584df87c1eeb839fe83aed8da67d2b405bd8d88
Bernardo Rodríguez Martín <rodriguezmartinbernardo@gmail.com>
2019-09-04 17:27:46 +0200

    consensusDb modifications in the fasta headers

--- consensusDb.py
+++ consensusDb.py
@@ -84,7 +84,7 @@ for sequenceId in FASTA.seqDict:
     if subfamily in targets:
 
         family = targets[subfamily]
-        newId = 'consensus|' + subfamily + '|' + family
+        newId = 'consensus|'+ family + '|' + subfamily  
         sequence = FASTA.seqDict[sequenceId]
 
         ## Remove trailing polyA from consensus sequence (Include polyA trimming as option)
