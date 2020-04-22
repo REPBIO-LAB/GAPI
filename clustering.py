bce66545b587b8600ff35c3899ed0cdda7939615
Bernardo Rodríguez Martín <rodriguezmartinbernardo@gmail.com>
2020-04-22 15:47:58 +0200

    Comment cluster removal to avoid code break

--- clustering.py
+++ clustering.py
@@ -262,8 +262,8 @@ def reciprocal_overlap_clustering(binDb, minPercOverlap, minClusterSize, eventTy
                     clustersDict[mergedCluster.id] = mergedCluster
 
                     ## Remove clusters that were merged from the clusters dictionary (Investigate later why if enabled the codes break)
-                    for cluster in clusters2merge:
-                        clustersDict.pop(cluster.id, None)
+                    #for cluster in clusters2merge:
+                    #    clustersDict.pop(cluster.id, None)
 
                 # C) No cluster overlaps A
                 else:
